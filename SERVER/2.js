0db3db398efaf7f03899cb08f8c7127fea1f3a5c
AdityaLodhiya <adityalodhiya111@gmail.com>
2026-04-05 16:13:49 +0530

    Delete SERVER directory

--- SERVER/2.js
+++
@@ -1,7 +0,0 @@
-http=require('http')
-http.createServer((req,res)=>{
-    obj={'name':"abc","age":24}
-    res.writeHead(200,{"Content-type":"application/JSON"})
-    
-    res.end(JSON.stringify(obj))
-}).listen(5678,()=>{console.log("Server Created");})
