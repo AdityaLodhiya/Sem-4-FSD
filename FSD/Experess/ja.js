eb9a26107d13c0bb2f19458b6cbd2a3590c52214
AdityaLodhiya <adityalodhiya111@gmail.com>
2026-04-24 08:47:05 +0530

    Delete FSD/Experess directory

--- FSD/Experess/ja.js
+++
@@ -1,9 +0,0 @@
-var express=require("express")
-var app=express()
-app.get("/",(req,res)=>{
-    res.type("text/html")
-    res.send("<h1>jay</h1>")
-})
-app.listen(3000,()=>{
-    console.log("start");
-})
